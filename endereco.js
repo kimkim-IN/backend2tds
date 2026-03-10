6262f47ab42c10da6dca8629d59145d4817cbc0e
kimkim-IN <kimberly.barnabe@escola.pr.gov.br>
2026-03-10 11:40:50 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,8 @@
+console.log("Endereço Completo");
+let rua = "Rua das Flores";
+let numero = 489;
+let cidade = "Cur de Curitiba";
+
+let endereco = rua + ", " + numero + " - " + cidade;
+//console.log(endereco);
+console.log("O endereço correto é: " + endereco);
