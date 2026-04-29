64b3836c9688c5210ada97c4fea59d8e462d2372
kimkim-IN <kimberly.barnabe@escola.pr.gov.br>
2026-04-29 13:30:41 +0000

    v1

---
+++ produto.js
@@ -0,0 +1,8 @@
+const produto = "Mochila Impermeavel";
+let preco = 189.90;
+let estoque = 14;
+const categoria = "Acessórios";
+
+console.log(`A ${produto} custa R$${preco.toFixed(2)}.`);
+console.log(`Temos ${estoque} itens no estoque.`);
+console.log(`O produto pertence a categoria ${categoria}.`);
