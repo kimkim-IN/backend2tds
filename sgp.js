b9a5e19859908578ad564cc9660d61261a222db2
kimkim-IN <kimberly.barnabe@escola.pr.gov.br>
2026-05-06 13:49:49 +0000

    v4

--- sgp.js
+++ sgp.js
@@ -26,8 +26,29 @@ function atualizarInventario(inventarioAtual, acao, ) {
     if (acao === "pegar"){
         const inventarioComItemNovo = [inventarioAtual, nomeDoItem];
         novoInventario = inventarioComItemNovo;
-    } else if (acao === "descartar")
-}
+    } else if (acao === "descartar") {
 
-const pontosDoJogador = [100, 50, 200, 10]
-console.log(pontosDoJogador);
+        const inventarioSemUltimoItem = inventarioAtual.slice();
+        novoInventario = inventarioSemUltimoItem;
+    } else {
+            novoInventario = inventarioAtual;
+         }
+         return novoInventario; // Retornar a mochila do jogador atualizada
+    };
+
+const pontosDoJogador = [400, 50, 250, 77]
+const total = calcularPontuacaoTotal(pontosDoJogador);
+console.log("Pontuação final: ", total);
+
+const missoes = [
+    {nome: "Salvar a aldeia", status: "Concluída"},
+    {nome: "Encontrar mapa", status: "Em andamento"},
+    {nome: "Derrotar o chefão", status: "Falhou"},
+    {nome: "Coletar recursos", status: "Concluída"}
+];
+
+const concluidas = filtrarMissoesConcluidas(missoes)
+console.log("Misões concluídas: ", concluidas);
+
+const mochila = ["Mapa", "Lanterna"];
+const mochilaAtualizada = atualizarInventario (mochila,"Pegar", "Bússula");
