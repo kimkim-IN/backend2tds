76ca8d601ce5d9c2df0f2bba3377a9b26efabdf9
kimkim-IN <kimberly.barnabe@escola.pr.gov.br>
2026-05-11 13:54:13 +0000

    v1

---
+++ teste.js
@@ -0,0 +1,20 @@
+//1. Declare 5 variáveis para representar uma viagem (destino, nome do viajante, cidade de origem, duração em dias e transporte)
+//2. Organizar uma forma de imprimir esses valores com interpolação ou saída individual
+
+const destino = "Nova York";
+const nomeViajante = "Keiran";
+const cidadeOrigem = "Sydney";
+let duracao = 22;
+let transporte = "Avião";
+
+console.log(`O viajante ${nomeViajante} deseja viajar de ${cidadeOrigem} até ${destino}, de ${transporte} em um percurso de ${duracao} horas. `);
+
+//3. Crie duas variá
+
+
+let hospedagem = 600;
+let alimentação = 300;
+
+let total = hospedagem + alimentação;
+
+console.log("O total dos gastos será: R$ " + total + "reais.");
