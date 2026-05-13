972459579d2ad2a2686785719337087d499317d7
kimkim-IN <kimberly.barnabe@escola.pr.gov.br>
2026-05-13 13:54:40 +0000

    versao1

---
+++ desafio.js
@@ -0,0 +1,9 @@
+//⚫declarar 5 variaveis para representar um funcionario
+
+const nomedaEmpresa = "Ferramentas Gerais";
+const nomeDaFuncionaria = "Nicolle";
+const IdadeDaFuncionaria = 23;
+let horasTrabalhadas = 10;
+let salario = 2.221;
+
+console.log(`A funcinaria ${nomeDaFuncionaria} tem ${IdadeDaFuncionaria} anos e trabalha na ${nomedaEmpresa} por ${horasTrabalhadas} horas e recebe ${salario} por mês.`)
